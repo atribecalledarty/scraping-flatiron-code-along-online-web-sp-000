83d31c23824413dfc5679023bdd137ce213418e4
Arthur Song <arth3rs0ng@gmail.com>
2019-04-09 05:13:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,12 @@ require_relative './course.rb'
 class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    doc.css(".post").each do |post|
+      course = Course.new
+      Course.title = post.css("h2").text
+      Course.schedule = post.css(".date").text
+      Course.description = post.css("p").text
+    end
     binding.pry
   end
   
