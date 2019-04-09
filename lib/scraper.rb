a0121916ad46c327d1708b0381f50c5b870727a3
Arthur Song <arth3rs0ng@gmail.com>
2019-04-09 05:17:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,6 +14,15 @@ class Scraper
     self.get_page.css(".post")
   end
   
+  def make_courses
+    self.get_courses.each do |course|
+      new_course = Course.new
+      new_course.title = course.css("h2").text
+      new_course.title = course.css(".date").text
+      new_course.title = course.css("p").text
+    end
+  end
+  
   
   def print_courses
     self.make_courses
