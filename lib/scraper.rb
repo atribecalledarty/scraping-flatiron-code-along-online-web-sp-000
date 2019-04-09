3e0581d04a94ccfc2e7e06172848442b632f6487
Arthur Song <arth3rs0ng@gmail.com>
2019-04-09 05:17:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,8 +18,8 @@ class Scraper
     self.get_courses.each do |course|
       new_course = Course.new
       new_course.title = course.css("h2").text
-      new_course.title = course.css(".date").text
-      new_course.title = course.css("p").text
+      new_course.schedule = course.css(".date").text
+      new_course.description = course.css("p").text
     end
   end
   
